a9e827d54dcc29f824450698c3f06c2ab36ac188
user <ccasbre27@hotmail.com>
2016-01-09 12:47:58 -0600

    The post detail view shows the info of the item passed

--- Pineapple/F3RPostDetailViewController.h
+++ Pineapple/F3RPostDetailViewController.h
@@ -12,5 +12,17 @@
 @interface F3RPostDetailViewController : UIViewController
 
 @property (weak, nonatomic) F3RCustomPost *post;
+@property (weak, nonatomic) IBOutlet UILabel *lblPostTitle;
+@property (weak, nonatomic) IBOutlet UIImageView *imgPost;
+@property (weak, nonatomic) IBOutlet UILabel *lblPostDate;
+@property (weak, nonatomic) IBOutlet UILabel *lblCategory;
+@property (weak, nonatomic) IBOutlet UILabel *lblSolvedStatus;
+@property (weak, nonatomic) IBOutlet UILabel *lblFollowStatus;
+@property (weak, nonatomic) IBOutlet UILabel *lblQuantityComments;
+@property (weak, nonatomic) IBOutlet UIImageView *imgType;
+@property (weak, nonatomic) IBOutlet UIImageView *imgSolvedStatus;
+@property (weak, nonatomic) IBOutlet UIImageView *imgFollowStatus;
+@property (weak, nonatomic) IBOutlet UIImageView *imgComment;
+@property (weak, nonatomic) IBOutlet UITextView *lblPostDescription;
 
 @end
--- Pineapple/F3RPostDetailViewController.m
+++ Pineapple/F3RPostDetailViewController.m
@@ -7,9 +7,12 @@
 //
 
 #import "F3RPostDetailViewController.h"
+#import <SDWebImage/UIImageView+WebCache.h>
 
 @interface F3RPostDetailViewController ()
-
+{
+    NSDateFormatter *dateFormat;
+}
 @end
 
 @implementation F3RPostDetailViewController
@@ -17,11 +20,53 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    dateFormat = [[NSDateFormatter alloc] init];
+    [dateFormat setDateFormat:@"dd/MM/yyyy"];
+
+    
+    // se cargan los datos
+    [self loadData];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void) loadData
+{
+    [_lblPostTitle setText:_post.nombre];
+    
+    [_imgPost sd_setImageWithURL:[NSURL URLWithString:_post.urlimagen]
+                    placeholderImage:[UIImage imageNamed:@"picture.png"]];
+    
+    [_lblPostDate setText:[NSString stringWithFormat:@"Fecha: %@",[dateFormat stringFromDate:_post.fechadesaparicion]]];
+    
+    [_lblPostDescription setText:_post.descripcion];
+    
+    // se verifica cual imagen se debe desplegar, se utiliza un operador ternario
+    NSString * typeNameImage = [_post.idestado isEqualToString:@"0"] ? @"found.png" : @"lost.png";
+    [_imgType setImage:[UIImage imageNamed:typeNameImage]];
+    
+    typeNameImage = [_post.idestado isEqualToString:@"0"] ? @"Encontrad@" : @"Perdid@";
+    [_lblCategory setText:typeNameImage];
+    
+    typeNameImage = [_post.solved isEqualToNumber:[NSNumber numberWithInt:0]] ? @"unsolved.png" : @"solved.png";
+    [_imgSolvedStatus setImage:[UIImage imageNamed:typeNameImage]];
+    
+    typeNameImage = [_post.solved isEqualToNumber:[NSNumber numberWithInt:0]] ? @"Pendiente" : @"Resuelto";
+    [_lblSolvedStatus setText:typeNameImage];
+    
+    
+    typeNameImage =  [_post.estado_follow isEqualToNumber:[NSNumber numberWithInt:0]] ? @"follow.png" : @"unfollow.png";
+    [_imgFollowStatus setImage:[UIImage imageNamed:typeNameImage]];
+    
+    typeNameImage =  [_post.estado_follow isEqualToNumber:[NSNumber numberWithInt:0]] ? @"Seguir" : @"Dejar de seguir";
+    [_lblFollowStatus setText:typeNameImage];
+    
+    [_lblQuantityComments setText:[NSString stringWithFormat:@"%@",_post.cantidad_comentarios]];
 }
 
 /*
